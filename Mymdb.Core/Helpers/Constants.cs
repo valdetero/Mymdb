ee3f3b50f685d2166019a60894e09914e5710e6a
valdetero <valdetero@users.noreply.github.com>
2014-07-30 17:19:41 -0500

    Update Constants.cs

--- Mymdb.Core/Helpers/Constants.cs
+++ Mymdb.Core/Helpers/Constants.cs
@@ -6,7 +6,7 @@ namespace Mymdb.Core
 	{
 		public struct TheMovieDatabase
 		{
-			public const string ApiKey = "300f55ca407beb4e9dd9960df16310a1";
+			public const string ApiKey = "<enter api key>";
 			public const string BasePath = "https://api.themoviedb.org/3/";
 			public const string ImagePath = "https://image.tmdb.org/t/p/w185";
 		}
